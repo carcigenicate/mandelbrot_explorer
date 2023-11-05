bb8bc119e5392237ed767a0b92937471482a02d5
carcigenicate <brendonw5@gmail.com>
2023-11-05 13:17:18 -0700

    Reduced ASCII-art char variety and added new coordinates.

--- src/ui/text.rs
+++ src/ui/text.rs
@@ -1,7 +1,8 @@
-use num::Complex;
 use crate::logic::mandelbrot_iteration;
 
-const DENSITY_SORTED_CHARS: &str = ".`-,:\'_;~*\"\\/^i!rl+|I=)(t<j>f1}{vx?L7z][JcTnuysYkohF4eaV3205pbqdXPZUC69K#AwHmg8E%&S$DORNGQBMW@";
+// Looks better with less chars for some reason
+//const DENSITY_SORTED_CHARS: &str = ".`-,:\'_;~*\"\\/^i!rl+|I=)(t<j>f1}{vx?L7z][JcTnuysYkohF4eaV3205pbqdXPZUC69K#AwHmg8E%&S$DORNGQBMW@";
+const DENSITY_SORTED_CHARS: &str = " .-:;~*\\/i({vx?@";
 
 fn char_for_iterations(n_iterations: u32, max_iterations: u32) -> char {
     let density_perc = n_iterations as f32 / (max_iterations + 1) as f32;
@@ -9,7 +10,7 @@ fn char_for_iterations(n_iterations: u32, max_iterations: u32) -> char {
     return DENSITY_SORTED_CHARS.as_bytes()[index] as char;
 }
 
-pub fn test_area_to_text(real_min: f64, real_max: f64, imag_min: f64, imag_max: f64, display_width: u32, display_height: u32, max_iterations: u32, infinity_limit: u32) -> String {
+pub fn draw_area(real_min: f64, real_max: f64, imag_min: f64, imag_max: f64, display_width: u32, display_height: u32, max_iterations: u32, infinity_limit: u32) -> String {
     let mut result = String::new();
 
     mandelbrot_iteration::test_area(real_min, real_max, imag_min, imag_max, display_width, display_height, max_iterations, infinity_limit, |real, imag, x, y, iters| {
